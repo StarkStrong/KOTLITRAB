123fd0c7a7893f91da83e155e8f432c6f674eaaa
Victor <56158657+StarkStrong@users.noreply.github.com>
2021-06-27 21:17:18 -0300

    KOTLINTRAB

---
+++ app/src/main/java/com/example/kotlintrab/ItemChat.kt
@@ -0,0 +1,10 @@
+package com.example.kotlintrab
+
+data class ItemChat(
+    val photo: String,
+    var username: String,
+    var age: Double,
+    var gender: String,
+    var sexualOrientation: String,
+    val city: String
+)
